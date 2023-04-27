dea560e212230e6318af4eb45afa623090dd4182
Sadaqat Ali <sadaqatali890@gmail.com>
2023-04-27 23:14:16 +0530

    mini ecommerce is added

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="hidden-xs">
+      <strong>Copyright &copy; 2023.</strong>
+      <b>All rights reserved</b>
+    </div>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer" style="background-color: darkgray">
+    <div class="container">
+      <div class="hidden-xs">
+        <b>Copyright &copy; 2023 All rights reserved</b>
+      </div>
+    </div>
+</footer>
